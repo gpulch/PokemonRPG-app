e7abef414b8d2a6ceb2a6a00eb2fe77f9837391e
gpulch <42658765+gpulch@users.noreply.github.com>
2023-01-14 00:46:38 +0100

    added movement zqsd

--- index.js
+++ index.js
@@ -13,8 +13,29 @@ image.src = "./img/Pokemon Style Game Map.png";
 const playerImage = new Image();
 playerImage.src = "./img/playerDown.png";
 
-image.onload = () => {
-  ctx.drawImage(image, -640, -350);
+class Sprite {
+  constructor({ position, velocity, image }) {
+    this.position = position;
+    this.image = image;
+  }
+  draw() {
+    ctx.drawImage(this.image, this.position.x, this.position.y);
+  }
+}
+
+const background = new Sprite({ position: { x: -640, y: -350 }, image: image });
+
+// animation
+const keys = {
+  up: { pressed: false },
+  down: { pressed: false },
+  left: { pressed: false },
+  right: { pressed: false },
+};
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  background.draw();
   ctx.drawImage(
     playerImage,
     0, // crop x
@@ -26,24 +47,51 @@ image.onload = () => {
     playerImage.width / 4, // player width
     playerImage.height // player height
   );
-};
+  if (keys.up.pressed && lastKey === keys.up) background.position.y += 3;
+  else if (keys.down.pressed && lastKey === keys.down)
+    background.position.y -= 3;
+  else if (keys.left.pressed && lastKey === keys.left)
+    background.position.x += 3;
+  else if (keys.right.pressed && lastKey === keys.right)
+    background.position.x -= 3;
+}
+animate();
 
 // movement
-
+let lastKey = null;
 window.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "z":
-      console.log("up");
+      keys.up.pressed = true;
+      lastKey = keys.up;
       break;
     case "q":
-      console.log("left");
+      keys.left.pressed = true;
+      lastKey = keys.left;
       break;
     case "s":
-      console.log("down");
+      keys.down.pressed = true;
+      lastKey = keys.down;
       break;
     case "d":
-      console.log("right");
+      keys.right.pressed = true;
+      lastKey = keys.right;
       break;
   }
-
+});
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "z":
+      keys.up.pressed = false;
+      break;
+    case "q":
+      keys.left.pressed = false;
+      break;
+    case "s":
+      keys.down.pressed = false;
+      break;
+    case "d":
+      keys.right.pressed = false;
+      break;
+  }
 });
